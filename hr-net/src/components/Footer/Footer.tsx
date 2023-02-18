7f47282629f55e580fa9e8ae7bc78925360a0bb2
Younes Lahouiti <loller763@gmail.com>
2023-02-18 20:27:46 +0100

    Ajout des images pour le header et le footer

--- hr-net/src/components/Footer/Footer.tsx
+++ hr-net/src/components/Footer/Footer.tsx
@@ -1,5 +1,24 @@
 import React from "react";
 
+//Next
+import Image from "next/image";
+
+//Images
+import footerIcon from "../../../public/Entir-new-icon 2.png";
+
 export default function Footer(): JSX.Element {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="footer">
+      {
+        <Image
+          src={footerIcon}
+          alt="Footer icon for the Wealth Health brand"
+          //Aspect ratio of 2.2/1
+          width={176}
+          height={80}
+          className="footer__image"
+        />
+      }
+    </footer>
+  );
 }
--- hr-net/src/components/Header/Header.tsx
+++ hr-net/src/components/Header/Header.tsx
@@ -1,5 +1,20 @@
 import React from "react";
 
+//Next
+import Image from "next/image";
+
+//Images
+import headerIcon from "../../../public/HRnet-logo.svg";
+
 export default function Header(): JSX.Element {
-  return <header>Header</header>;
+  return (
+    <header className="header">
+      <Image
+        src={headerIcon}
+        alt="Header with the logo of the HRnet brand"
+        height={90}
+        width={90}
+      />
+    </header>
+  );
 }
